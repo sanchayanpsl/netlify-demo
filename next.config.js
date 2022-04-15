7ade11f5a372819a052fbe43a2cc59dd33fa11e7
Sanchayan Das <sanchayan_das@persistent.com>
2022-04-15 14:43:31 +0530

    code update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     serverRuntimeConfig: {
-        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+        secret: 'sd5egdf5ef35f3fdd'
     },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
